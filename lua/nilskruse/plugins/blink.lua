40ab36b41112f93b0e3702f868a9b69036aa7ebd
Nils Kruse <nilskruse97@gmail.com>
2025-05-24 16:24:43 +0200

    Some adjustments

--- lua/nilskruse/plugins/blink.lua
+++ lua/nilskruse/plugins/blink.lua
@@ -20,12 +20,12 @@ return { -- Autocompletion
 				-- `friendly-snippets` contains a variety of premade snippets.
 				--    See the README about individual language/framework/plugin snippets:
 				--    https://github.com/rafamadriz/friendly-snippets
-				-- {
-				--   'rafamadriz/friendly-snippets',
-				--   config = function()
-				--     require('luasnip.loaders.from_vscode').lazy_load()
-				--   end,
-				-- },
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
 			},
 			opts = {},
 		},
@@ -75,7 +75,12 @@ return { -- Autocompletion
 		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "lazydev" },
+			default = {
+				"lsp",
+				"path",
+				"snippets",
+				"lazydev",
+			},
 			providers = {
 				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 			},
@@ -90,7 +95,7 @@ return { -- Autocompletion
 		-- the rust implementation via `'prefer_rust_with_warning'`
 		--
 		-- See :h blink-cmp-config-fuzzy for more information
-		fuzzy = { implementation = "lua" },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 
 		-- Shows a signature help window while you type arguments for a function
 		signature = { enabled = true },
--- lua/nilskruse/plugins/conform.lua
+++ lua/nilskruse/plugins/conform.lua
@@ -35,6 +35,8 @@ return { -- Autoformat
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+
+			rust = { "rustfmt", lsp_format = "fallback" },
 		},
 	},
 }
